35c51447dcbd87b2656f98d1e4c2dadea9cd8b47
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-17 00:04:24 +0500

    feat: models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from common.models import BaseModel
 
-# Create your models here.
+
+class TelegramAdmin(BaseModel):
+    chat_id = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.chat_id}"
