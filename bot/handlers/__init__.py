47fefea8702fa2642a7ccfdbdf3addbedae666bb
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-17 08:17:10 +0500

    feat: help handler

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,12 +2,13 @@ from aiogram import Router
 
 
 def get_handlers_rounter() -> Router:
-    from . import start, admin, chat, post
+    from . import start, admin, chat, post, help
 
     router = Router()
     router.include_router(start.router)
     router.include_router(admin.router)
     router.include_router(chat.router)
     router.include_router(post.router)
+    router.include_router(help.router)
 
     return router
---
+++ bot/handlers/help.py
@@ -0,0 +1,27 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from bot.core.loader import bot
+
+
+router = Router(name="help")
+
+
+@router.message(Command("help"))
+async def command_help_handler(message: Message):
+    # help message to give full information about the bot
+
+    bot_info = await bot.get_me()
+
+    message_text = (
+        "Bot haqida batafsil ma'lumot:\n\n"
+        f"🤖 Botning ismi: {bot_info.full_name}\n\n"
+        f"👤 Botning username: @{bot_info.username}\n"
+        "Botni ishlatish uchun quyidagi buyruqlardan foydalanishingiz mumkin:\n\n"
+        "/start - Botni ishga tushirish\n"
+        "/admin - Admin paneliga kirish\n"
+        "/chat - Chatlar ro'yxatini ko'rish\n"
+        "/post - Post yaratish\n\n"
+    )
+    await message.answer(message_text)
