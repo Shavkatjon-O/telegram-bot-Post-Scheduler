e269846ae6e0dc5c36ccc90e38187a405c46afa2
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-19 00:00:55 +0500

    fix

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -53,5 +53,7 @@ async def admin_create_user_handler(message: Message, state: FSMContext) -> None
 
     await message.answer(text=message_text)
 
+    # TODO: fix this code
+
     await state.clear()
     await command_start_handler(message, state)
