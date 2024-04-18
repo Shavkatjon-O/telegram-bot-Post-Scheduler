2e847b15fb81a23e5e9af5fddb5137a80437c77b
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-18 11:35:30 +0500

    feat: create or delete

--- bot/handlers/chat.py
+++ bot/handlers/chat.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.filters import Command
+from aiogram.filters import Command, or_f
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
@@ -29,3 +29,25 @@ async def return_to_menu_handler(message: Message, state: FSMContext) -> None:
 
     await state.clear()
     await command_start_handler(message, state)
+
+
+@router.message(
+    ChatStates.CHAT,
+    or_f(F.text == ChatKeyboard.CREATE, F.text == ChatKeyboard.DELETE),
+)
+async def create_or_delete_handler(message: Message, state: FSMContext) -> None:
+    """Handler for creating or deleting chats."""
+
+    message_text = "Kanal @nomini kiriting 📝"
+    await message.answer(text=message_text, reply_markup=CancelKeyboard.get_keyboard())
+
+    await state.update_data(action=message.text)
+    await state.set_state(ChatStates.CREATE_OR_DELETE)
+
+
+@router.message(ChatStates.CREATE_OR_DELETE, F.text == CancelKeyboard.CANCEL)
+async def cancel_action_handler(message: Message, state: FSMContext) -> None:
+    """Handler for cancelling the action."""
+
+    await state.clear()
+    await command_chat_handler(message, state)
