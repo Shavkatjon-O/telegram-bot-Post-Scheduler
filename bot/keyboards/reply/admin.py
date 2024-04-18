a1d8dc7bea0fae422c7b312871f2291cc33c940f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-04-18 12:29:55 +0500

    feat: admin menu keyboard

--- bot/keyboards/reply/admin.py
+++ bot/keyboards/reply/admin.py
@@ -2,10 +2,10 @@ from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, KeyboardButton
 
 
-class AdminKeyboards:
-    CREATE = "Yaratish ➕"
-    DELETE = "O'chirish ➖"
-    MENU = "Asosiy menyu 🏠"
+class AdminMenuKeyboard:
+    CREATE = "Qo'shish 📝"
+    DELETE = "O'chirish 🗑"
+    MENU = "Asosiy menu 🏠"
 
     @classmethod
     def get_keyboard(cls) -> ReplyKeyboardMarkup:
@@ -14,22 +14,7 @@ class AdminKeyboards:
             [KeyboardButton(text=cls.DELETE)],
             [KeyboardButton(text=cls.MENU)],
         ]
-
         keyboard = ReplyKeyboardBuilder(markup=buttons)
         keyboard.adjust(2, 1)
-        return keyboard.as_markup(
-            resize_keyboard=True,
-        )
 
-
-class CancelKeyboard:
-    CANCEL = "Bekor qilish ❌"
-
-    @classmethod
-    def get_keyboard(cls) -> ReplyKeyboardMarkup:
-        return ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text=cls.CANCEL)],
-            ],
-            resize_keyboard=True,
-        )
+        return keyboard.as_markup(resize_keyboard=True)
